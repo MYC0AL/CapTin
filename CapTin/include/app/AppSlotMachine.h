af2393393e8ec27c7c5ee46198beddaf32f3b266
Michael Bottita <michaeljbottita@gmail.com>
2025-07-04 12:47:57 -0700

    CapTin: Added state machine to slots
    - Working credit system, need to add
    logic for winning payouts
    - Working lock system
    - Need to add bet amounts and buttons

--- CapTin/include/app/AppSlotMachine.h
+++ CapTin/include/app/AppSlotMachine.h
@@ -16,9 +16,6 @@
 #include "cmn/ControlDisplay.h"
 #include "cmn/ControlTouch.h"
 #include "arduino_sprite.h"
-#include "sprites/m.h"
-#include "sprites/a.h"
-#include "sprites/man.h"
 #include "sprites/reel.h"
 #include <stdlib.h>
 #include <iostream>
@@ -26,10 +23,47 @@
 /**********************
  * Defines
  **********************/
+#define REEL_CNT    3
+#define REEL_WIDTH  128
+#define SLOT_MID    150
 
 /**********************
  * Types
  **********************/
+typedef enum
+{
+    SLOT_STATE_IDLE,
+    SLOT_STATE_LOAD,
+    SLOT_STATE_SPIN,
+    SLOT_STATE_LOCK,
+    SLOT_STATE_ITEM,
+    SLOT_STATE_COIN,
+} SlotState_t;
+
+typedef uint8_t SlotItems_t;
+enum 
+{
+    SLOT_ITEM_SEVEN = 0,
+    SLOT_ITEM_BELL,
+    SLOT_ITEM_ORANGE,
+    SLOT_ITEM_BANNANA,
+    SLOT_ITEM_BAR,
+    SLOT_ITEM_GRAPE,
+    SLOT_ITEM_MELON,
+    SLOT_ITEM_LEMON,
+    SLOT_ITEM_CHERRY
+};
+
+typedef struct
+{
+    Arduino_Sprite* sprite;
+    int             scroll;     /* How far the sprite has scrolled */
+    int             scroll_sp;  /* How fast the sprite scrolls     */
+    uint8_t         scroll_tm;  /* How long the sprite scrolls     */
+    SlotState_t     state;      /* State the reel is in            */
+    SlotItems_t     item;       /* Currently selected item         */
+
+} SlotReel_s;
 
 /**********************
  * Variables
--- CapTin/src/app/AppSlotMachine.cpp
+++ CapTin/src/app/AppSlotMachine.cpp
@@ -17,11 +17,18 @@
 /**********************
  * Function Prototypes
  **********************/
+static void Slot_InitReels( );
+static void Slot_LoadReels( );
+static void Slot_SpinReels( );
+static void Slot_LockReels( );
+static void Slot_UpdtItems( );
+static void Slot_UpdtCoins( );
 
 /**********************
  * Variables
  **********************/
-
+static SlotReel_s reel[ REEL_CNT ] = {0};
+static int        coins = 100;
 /**********************
  * Functions
  **********************/
@@ -35,68 +42,201 @@ void SlotMachine_run( void * pvParameters )
 {
     Serial.println("SlotMachine: Application Started ");
 
-    // TODO MJB: Use https://github.com/h5n1xp/Arduino_Sprite?tab=readme-ov-file ?
-
-    int aAframe = 0;
-    int scroll = 0;
-    Arduino_Sprite* a;
-
-    Arduino_Sprite* reel1;
-    Arduino_Sprite* reel2;
-    Arduino_Sprite* reel3;
+    // https://github.com/h5n1xp/Arduino_Sprite?tab=readme-ov-file
 
     Arduino_ST7701_RGBPanel * gfx = Display_getGFX();
-
     Arduino_GFX* canvas = Display_getCanvas();
-
-    canvas->fillScreen(BLACK);
-    canvas->flush();
 
     Display_getGFX()->fillScreen(BLACK); // Note: Do not call gfx->begin() when using canvas
 
-    reel1 = new Arduino_Sprite(128, 300, canvas);
-    reel1->Move(15,0);
-    reel1->begin((uint8_t*)imageReel,paletteReel);
-    reel1->initAnim(1,0,0,128,1152);
+    Slot_InitReels();
 
-    reel2 = new Arduino_Sprite(128, 300, canvas);
-    reel2->Move(15+128+15,0);
-    reel2->begin((uint8_t*)imageReel,paletteReel);
-    reel2->initAnim(1,0,0,128,1152);
-
-    reel3 = new Arduino_Sprite(128, 300, canvas);
-    reel3->Move(15+128+15+128+15,0);
-    reel3->begin((uint8_t*)imageReel,paletteReel);
-    reel3->initAnim(1,0,0,128,1152);
+    uint8_t touch_count = 0;
+    TP_Point touches[TOUCH_MAX] = {};
 
     while(1)
-    {
-        scroll += 30;
+    {   
+        Touch_getTouches( touches, &touch_count );
 
-        if( scroll > 1152 )
+        if ( touch_count > 0 )
         {
-            scroll = 0;
+            Slot_LoadReels();
         }
-        
-        reel1->Move(15,scroll);
-        reel1->ScrollV();
 
-        reel2->Move(15+128+15,scroll);
-        reel2->ScrollV();
-
-        reel3->Move(15+128+15+128+15,scroll);
-        reel3->ScrollV();
-
-        //reel1->Frame(aAframe);
-        // reel2->Frame(aAframe);
-        //reel3->Frame(aAframe);
-
-
-        //reel1->Draw();
-        // reel2->Draw();
-        //reel3->Draw();
-
-        canvas->flush();
-        //gfx->print("test");
+        Slot_SpinReels();
+        Slot_LockReels();
+        Slot_UpdtItems();
+        Slot_UpdtCoins();
     }
+}
+
+/***************************************************
+ * Slot_InitReels()
+ * 
+ * Description: Initialize the reels 
+ **************************************************/
+static void Slot_InitReels( )
+{
+    Serial.println("Initializing Reels");
+    Arduino_GFX * canvas = Display_getCanvas();
+
+    canvas->fillScreen(BLACK);
+
+    for ( int i = 0; i < REEL_CNT; i++ )
+    {
+        reel[i].sprite = new Arduino_Sprite(128, 300, canvas);  /* Create a new sprite object              */
+        reel[i].sprite->initAnim(1,0,0,128,1152);               /* Initialize the reel animation           */
+        reel[i].sprite->begin((uint8_t*)imageReel,paletteReel); /* Initialize the sprites with their image */
+        reel[i].sprite->Move((i*128)+(15*i)+15,0);              /* Move the reels into position            */
+        reel[i].sprite->ScrollV();                              /* Scrolling here will draw the sprites    */
+        reel[i].state = SLOT_STATE_LOAD;
+    }
+
+    canvas->flush();                                            /* Flushing the canvas will draw it         */
+}
+
+/***************************************************
+ * Slot_LoadReels()
+ * 
+ * Description: Ready the reels for spinning
+ **************************************************/
+static void Slot_LoadReels( )
+{
+    for ( int i = 0; i < REEL_CNT; i++ )
+    {
+        if ( reel[i].state == SLOT_STATE_LOAD )
+        {
+            reel[i].scroll_tm = 85 - (i*15);
+            reel[i].scroll_sp = 80 - (i*10);
+            reel[i].state = SLOT_STATE_SPIN;
+            //Serial.printf("Loaded Reel %d\r\n",i);
+        }
+    }
+}
+
+/***************************************************
+ * Slot_SpinReels()
+ * 
+ * Description: Spin the reels
+ **************************************************/
+static void Slot_SpinReels( )
+{
+    //Serial.println("Spinning Reels");
+    Arduino_GFX * canvas = Display_getCanvas();
+
+    for (int i = 0; i < REEL_CNT; i++)
+    {
+        if ( reel[i].state == SLOT_STATE_SPIN )
+        {
+            if ( reel[i].scroll_tm > 0 )
+            {
+                reel[i].scroll = reel[i].scroll + reel[i].scroll_sp > 1152 ? 0 : reel[i].scroll + reel[i].scroll_sp;
+                reel[i].sprite->Move(reel[i].sprite->GetX(),reel[i].scroll);
+                reel[i].sprite->ScrollV();
+                reel[i].scroll_tm--;
+                reel[i].scroll_sp = reel[i].scroll_sp < 0 ? 0 : reel[i].scroll_sp - 1;
+            }
+            else
+            {
+                reel[i].state = SLOT_STATE_LOCK;
+                //Serial.printf("Reel %d finished spinning, trans to lock\r\n",i);
+            }
+        }
+    }
+
+    canvas->flush();
+}
+
+/***************************************************
+ * Slot_LockReels()
+ * 
+ * Description: After reels have spun, lock them
+ *              in place.
+ **************************************************/
+static void Slot_LockReels( )
+{
+    //Serial.println("Locking Reels");
+    /* Figure out displacement of each reel */
+    /* If a reel is already locked, skip it */
+    int reel_disp[ REEL_CNT ] = {0};
+    for ( int i = 0; i < REEL_CNT; i++ )
+    {
+        if ( reel[i].state == SLOT_STATE_LOCK )
+        {
+            reel_disp[i] = reel[i].sprite->GetY() % REEL_WIDTH;
+            if ( reel_disp[i] == 0 )
+            {
+                reel[i].state = SLOT_STATE_ITEM;
+                //Serial.printf("Reel %d finished locking, trans to ITEM\r\n",i);
+            }
+        }
+    }
+
+    /* Process all reels ready for alignment */
+    for ( int i = 0; i < REEL_CNT; i++ )
+    {
+        if ( reel[i].state == SLOT_STATE_LOCK )
+        {
+            int tmp_sp = reel_disp[i] > (REEL_WIDTH * 0.65) ? REEL_WIDTH - reel_disp[i] : -reel_disp[i];
+            reel[i].scroll_sp = tmp_sp;
+            reel[i].scroll_tm = 1;
+            reel[i].state = SLOT_STATE_SPIN;
+        }
+    }
+}
+
+/***************************************************
+ * Slot_UpdtItems()
+ * 
+ * Description: Update which item is selected on
+ *              each reel.
+ **************************************************/
+static void Slot_UpdtItems( )
+{
+    //Serial.println("Updating reel items");
+    for ( int i = 0; i < REEL_CNT; i++ )
+    {
+        if ( reel[i].state == SLOT_STATE_ITEM )
+        {
+            reel[i].item = reel[i].sprite->GetY() / REEL_WIDTH;
+            reel[i].state = SLOT_STATE_COIN;
+            //Serial.printf("Reel %d finished updating item, trans to COIN\r\n",i);
+        }
+    }
+}
+
+/***************************************************
+ * Slot_UpdtCoins()
+ * 
+ * Description: Update user coin count
+ **************************************************/
+static void Slot_UpdtCoins( )
+{
+    /* Verify all reels are locked and their items are updated */
+    bool rdy = true;
+    for ( int i = 0; i < REEL_CNT & rdy; i++ )
+    {
+        if ( reel[i].state != SLOT_STATE_COIN )
+        {
+            rdy = false;
+            return;
+        }
+    }
+
+    //Serial.println("All reels ready, calculating winnings");
+
+    /* Update coin count if win */
+    for ( int i = 0; i < REEL_CNT & rdy; i++ )
+    {
+        reel[i].state = SLOT_STATE_LOAD;
+    }
+
+    coins++;
+
+    Arduino_ST7701_RGBPanel * gfx = Display_getGFX();
+    gfx->setCursor(275,425);
+    gfx->setTextSize(2);
+    gfx->setTextColor(ORANGE);
+    gfx->fillRect(275,425,160,20,BLACK);
+    gfx->printf("Credits: %d",coins);
 }
